ae5d73554d4b108fc27624603e432fcb777c3d3b
meninozinhomanso <39411695+meninozinhomanso@users.noreply.github.com>
2018-06-12 11:00:59 -0300

    Create CodigosDasCarinhas3.js

---
+++ CodigosDasCarinhas3.js
@@ -0,0 +1,502 @@
+var normal="abcdefghijklmnopqrstuvwxyz!?*<>.,=+-()0123456789";
+var flipping="ɐqɔpǝɟƃɥıſʞןɯuodbɹsʇnʌʍxʎz¡¿*<>.,=+-()0⇂ՇƐhʢ9L86";
+var weirdone="αßςdεƒghïյκﾚmη⊕pΩrš†u∀ωxψz!?★◄►01ՁკЧƼбלȣף";
+var slamm="αвc∂єƒgнιנкℓмησρqяѕтυνωχуz!؟●«».,=+-()012З456789";
+var loveletterscombinedletters="ΛßƇDƐFƓĤĪĴҠĿMИ♡ṖҨŔSŦƱ√ѠӾYZ!?★+◄►01ՁკЧƼбלȣף"
+var kringel="ªb¢ÞÈF૬ɧÎjΚĻмη◊ǷƍrS⊥µ√w×ýz!?*★<>Θı2ʓ45бך89";
+var japan="ﾑ乃ζÐ乇ｷǤんﾉﾌズﾚᄊ刀ԾｱQ尺丂ｲЦЏЩﾒﾘ乙!?*+()ᅙ1ᆯЗ456ᆨ89";
+var tamilisch="ԹՅՇԺȝԲԳɧɿʝƙʅʍՌԾρφՐՏԵՄעաՃՎՀ!?*+<>θΙՁკЧƼбלȣף";
+var busted="ǞвटDęբg৸ijκlɱПΦРqЯsƮЦvЩжყւ!?*+()012ʓ45б789";
+var mixed="ªb¢ÞÈF♀ޔÎjΚ¦∞η◊pÕrS⊥µ√w×ýz!?★◄►01ՁკЧƼбלȣף";
+var freshed="Λɓ¢Ɗ£ƒɢɦĩʝҚŁɱהøṖҨŔŞŦŪƔωχ¥Ẑ!?*+<>0123456789"
+var groß="ÁßČĎĔŦĞĤĨĴĶĹМŃŐРQŔŚŤÚVŴЖŶŹ!?★+◄►01ՁკЧƼбלȣף"
+var devil="ⓐßʗðⓔƒɠнìʖҜℓмиѺρҨяƧ†µƔŴӾ¥ẕ!?★♪◄►01ՁკЧƼбלȣף"
+var skinny="ᵃᵇᶜᵈᵉᶠᵍʰᶤʲᵏˡᵐᶰᵒᵖᵠʳˢᵗᵘᵛʷˣʸᶻᵎ﹖*<>ˑ,⁼⁺⁻⁽⁾₀₁₂₃₄₅₆₇₈₉";
+var symbo="Λв¢∂єƒgнιנкℓмηΩρqяѕтυνωχуz!?★+◄►01ՁკЧƼбלȣף"
+var kinky="ΛBCDΣFGΉIJΚŁMПOPQЯSTЦVШXΫZ!?*+<>0123456789"
+var arabia="äßçÐعFgђ¡ʖkℓм₪٥ÞQя§тµ√ШҲγž!?*+<>0123456789"
+var leet="4BCD3FGH1JKLMN0PQR57UVXWYZ!?♪★◄►01ՁკЧƼбלȣף"
+var oldo="ค๒ς๔єŦﻮђเןкl๓ภ๏קợгรtยשฬץאz!؟●★«»01ՁკЧƼбלȣף"
+var semi="ábćdéfghíjklmńőpqŕśtúvwxýź!?★♪◄►01ՁკЧƼбלȣף"
+var changed="αв¢∂єƒgнιנкℓмησρqяѕтυνωχуz!?♪★◄►01ՁკЧƼбלȣף"
+var egypt="αβcδεŦĝhιjκlʍπøρφƦ$†uυωχψz!?♪★◄►01ՁკЧƼбלȣף"
+var zot="ΛБϾÐΞŦGҢł|КŁMΠØPǪΓSƮЦVЩЖЧZ!?★★◄►01ՁკЧƼбלȣף"
+var balls="ⓐⓑ©ⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ!?★+◄►01ՁკЧƼбלȣף"
+var ballsbig="ⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏ?★+◄►01ՁკЧƼбלȣף"
+var loveletterscombined="ɑвς∂єƒɢɦɨʆќℓʍиღρҩʀsтµνωхγẕ?*+<>0123456789"
+var crank="ἇḇϾÐἕŦ₲ḩł|₭ŁMΠØ₱ǪΓ₰₮ὒ√ⱲЖὟZ!?★+◄►01ՁკЧƼбלȣף"
+var bigbold="ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ！？＊＜＞．，＝＋－（）０１２３４５６７８９";
+var written="αႦƈԃҽϝɠԋιʝƙʅɱɳσρϙɾʂƚυʋɯxყȥǃʔ*<>.,=+-()૦١Ձ૩મƼେ7Ց୨";
+var swirls="คც८ძ૯Բ૭ҺɿʆқՆɱՈ૦ƿҩՐς੮υ౮ω૪עઽ!ʔ*<>.,=+-()૦1૨૩456ך89";
+var flowers="ᾄвƈḋἔғʛђἷʝќłмᾗὄῥqʀṩҭὗvᾧẋẏẓ!?*<>.,=+-()012З456789";
+var techto="ΛBᄃDΣFGΉIJΚᄂMПӨPQЯƧƬЦVЩXΥZ!?*◁▷.,=+-()"
+var classic="αвcdeғɢнιjĸlмɴopqrѕтυvwхyz!?*<>.,=+-()012З456789"
+var uppercase="ᴀвcᴅᴇғɢнıנκʟмɴσρǫʀsтυνωхʏz!?*<>.,=+─()૦١Ձ૩મƼେ7Ց୨"
+var ranged="ﾑ乃ㄈの乇ｷǤけﾉﾌҜしɱ刀ѺㄕҨ尺らｲŲ√￦ﾒㄚ乙?*<>";
+
+
+function change(_in, _out, _let, _old, _egi, _spk, _bal, _bab, _lov, _cra, _lie,  _wig, _jap, _tam, _sla, _coo, _big, _mag, _wes, _ara, _dev, _bbold, _skin, _writt, _swir, _flows, _tech, _rang, _flpp, _fkk1, _fkk2)
+{
+  var s="", le="", ol="", eg="", sp="", ba="",  bb="", lo="",  cr="", li="", wi="", ja="", ta="", sl=""; co=""; bi=""; ma=""; we=""; ar=""; de=""; bgb=""; ski=""; wri=""; swi=""; flow=""; tec=""; ran=""; flp=""; fk1=""; fk2="";
+  var n=_in.value.toLowerCase();
+ 
+  if (_in.value.length==0) return;
+  
+  for(i=0; i<n.length; i++)
+  {
+    var c=n.charAt(i);
+    for(j=0; (j<normal.length)&&(c!=normal.charAt(j)); j++);
+    if (j<normal.length) { 
+      s+=changed.charAt(j); le+=leet.charAt(j); ol+=oldo.charAt(j); eg+=egypt.charAt(j); sp+=zot.charAt(j); ba+=balls.charAt(j);  bb+=ballsbig.charAt(j); lo+=loveletterscombined.charAt(j);cr+=crank.charAt(j);li+=loveletterscombinedletters.charAt(j);wi+=kringel.charAt(j);ja+=japan.charAt(j);ta+=tamilisch.charAt(j);sl+=busted.charAt(j);co+=freshed.charAt(j);bi+=groß.charAt(j);ma+=symbo.charAt(j);we+=kinky.charAt(j);ar+=arabia.charAt(j);de+=devil.charAt(j);bgb+=bigbold.charAt(j);ski+=skinny.charAt(j);wri+=written.charAt(j);swi+=swirls.charAt(j);flow+=flowers.charAt(j);tec+=techto.charAt(j);ran+=ranged.charAt(j);flp+=flipping.charAt(j);fk1+=classic.charAt(j);fk2+=uppercase.charAt(j)} else {
+      s+=c; le+=c; ol+=c; eg+=c; sp+=c; ba+=c; bb+=c; lo+=c; cr+=c; li+=c; wi+=c; ja+=c; ta+=c; sl+=c; co+=c; bi+=c; ma+=c; we+=c; ar+=c; de+=c; bgb+=c; ski+=c; wri+=c; swi+=c; flow+=c; tec+=c; ran+=c; flp+=c; fk1+=c; fk2+=c;
+    }
+  }
+  _out.value=s;
+  _let.value=le;
+  _old.value=ol;
+  _egi.value=eg;
+  _spk.value=sp;
+  _bal.value=ba;
+  _bab.value=bb;
+  _lov.value=lo;
+  _cra.value=cr;
+  _lie.value=li;
+  _wig.value=wi;
+  _jap.value=ja;
+  _tam.value=ta;
+  _sla.value=sl;
+  _coo.value=co;
+  _big.value=bi;
+  _mag.value=ma;
+  _wes.value=we;
+  _ara.value=ar;
+  _dev.value=de;
+  _bbold.value=bgb;
+  _skin.value=ski;
+   _writt.value=wri;
+   _swir.value=swi;
+   _flows.value=flow;
+   _tech.value=tec;
+   _rang.value=ran;
+   _flpp.value=flp;
+   _fkk1.value=fk1;
+     _fkk2.value=fk2;
+}
+
+ function doClear(theText) {
+     if (theText.value == theText.defaultValue) {
+         theText.value = ""
+     }
+ }
+
+function SelectAll(id)
+{
+    document.getElementById(id).focus();
+    document.getElementById(id).select();
+}
+function breezah(_in, _out)
+{
+	var s="";
+	var n=_in.value.toLowerCase();
+	
+	for(i=0; i<n.length; i++)
+	{
+	var c=n.charAt(i);
+	s+=i%2?c:c.toUpperCase();
+	}
+	  
+_out.value=s;
+}
+
+function textuber() {
+	var text = document.forms['nickstyler'].textin.value;
+	var result = new Array();
+	for (var i = 0; i <= text.length-1; i++) {
+		result[i] = '\u0305'+text[i]+'\u0305';
+	}
+	document.forms['nickstyler'].ueberstrichen.value = result.join('');
+}
+
+function textdurch() {
+	var text = document.forms['nickstyler'].textin.value;
+	var result = new Array();
+	for (var i = 0; i <= text.length-1; i++) {
+		result[i] = '\u0336'+text[i]+'\u0336';
+	}
+	document.forms['nickstyler'].durchgestrichen.value = result.join('');
+}
+
+function textunter() {
+	var text = document.forms['nickstyler'].textin.value;
+	var result = new Array();
+	for (var i = 0; i <= text.length-1; i++) {
+		result[i] = '\u0332'+text[i]+'\u0332';
+	}
+	document.forms['nickstyler'].unterstrichen.value = result.join('');
+}
+function BoxedString(aString) {
+ var last = aString.length - 1;
+ var result = new Array(aString.length)
+ for (var i = last; i >= 0; --i) {
+  var c = aString.charAt(i)
+  var r = BoxedTable[c]
+  result[i] = r != undefined ? r : c
+ }
+
+ return result.join('')
+
+}
+
+var BoxedTable = {
+a : '[̲̅a̲̅]',
+b : '[̲̅b̲̅]',
+c : '[̲̅c̲̅]',
+d : '[̲̅d̲̅]',
+e : '[̲̅e̲̅]',
+f : '[̲̅f̲̅]',
+g : '[̲̅g̲̅]',
+h : '[̲̅h̲̅]',
+i : '[̲̅i̲̅]',
+j : '[̲̅j̲̅]',
+k : '[̲̅k̲̅]',
+l : '[̲̅l̲̅]',
+m : '[̲̅m̲̅]',
+n : '[̲̅n̲̅]',
+o : '[̲̅o̲̅]',
+p : '[̲̅p̲̅]',
+q : '[̲̅q̲̅]',
+r : '[̲̅r̲̅]', 
+s : '[̲̅s̲̅]',
+t : '[̲̅t̲̅]',
+u : '[̲̅u̲̅]',
+v : '[̲̅v̲̅]',
+w : '[̲̅w̲̅]',
+x : '[̲̅x̲̅]',
+y : '[̲̅y̲̅]',
+z : '[̲̅z̲̅]',
+0 : '[̲̅0̲̅]',
+1 : '[̲̅1̲̅]',
+2 : '[̲̅2̲̅]',
+3 : '[̲̅3̲̅]',
+4 : '[̲̅4̲̅]',
+5 : '[̲̅5̲̅]',
+6 : '[̲̅6̲̅]',
+7 : '[̲̅7̲̅]',
+8 : '[̲̅8̲̅]',
+9 : '[̲̅9̲̅]',
+'!' : '[̲̅﹗̲̅]',
+'?' : '[̲̅﹖̲̅]'
+}
+
+for (i in BoxedTable) {
+  BoxedTable[BoxedTable[i]] = i
+}
+
+function textbox() {
+ 	var result = BoxedString(document.forms['nickstyler'].textin.value.toLowerCase()); document.forms['nickstyler'].box.value = result;
+}
+function BigString(aString) {
+ var last = aString.length - 1;
+ var result = new Array(aString.length)
+ for (var i = last; i >= 0; --i) {
+  var c = aString.charAt(i)
+  var r = BigTable[c]
+  result[i] = r != undefined ? r : c
+ }
+
+ return result.join('')
+
+}
+
+var BigTable = {
+a : '|̶̿ ̶̿ ̶̿ ̶̿|',
+b : '|̶͇̿ ̶͇̿ ̶͇̿)',
+c : '|͇̿ ͇̿ ͇̿ ͇̿ ',
+d : '|͇̿ ͇̿ ͇̿ ͇̿)',
+e : '|̶͇̿ ̶͇̿ ͇̿ ',
+f : '|̶̿ ̶̿ ̶̿ ̶̿ ',
+g : '|͇̿ ͇̿ ͇̿ ̶͇̿ι ',
+h : '|̶ ̶ ̶ ̶| ',
+i : '| ',
+j : '͇ ͇͇͇͇|',
+k : '|< ',
+l : '|͇ ͇  ',
+m : '|̿ V ̿|',
+n : '|̿ \\͇|',
+o : '|͇̿ ͇̿ ͇̿|',
+p : '|̶̿ ̶̿ ̶̿ ̶̿\' ',
+q : '|͇̿ ͇̿ ͇̿ ͇͇̿|͇ ',
+r : '|̿ ̿ ̿\\ ', 
+s : '͇ ͇\\̿ ̿ ',
+t : '  ̿ ̿|̿ ̿ ',
+u : '|͇ ͇ ͇ ͇ ͇|',
+v : '\\ ͇ / ',
+w : '|͇ Λ ͇|',
+x : 'X ',
+y : 'Y ',
+z : '̿ ̿/ ͇ ͇ '
+}
+
+for (i in BigTable) {
+  BigTable[BigTable[i]] = i
+}
+
+String.prototype.reverse = function() {
+var r = '';
+for(var i=this.length-1;i>=0;i--) {r += this.charAt(i);}return r;}
+function invert(n) {
+var field = document.getElementsByName(n)[0];
+field.value = field.value.reverse();
+}
+
+function textbig() {
+ 	var result = BigString(document.forms['nickstyler'].textin.value.toLowerCase()); document.forms['nickstyler'].big.value = result;
+}
+
+function SetBookmark() { 
+
+title = "www.Nickstyler.de - Nur das Beste für deine Nicks!";  
+url = "http://www.nickstyler.de"; 
+ 
+if (window.sidebar) { 
+
+// ADD Mozilla Firefox	
+window.sidebar.addPanel(title, url,"");	
+} else if( window.external ) { 
+
+// ADD IE Favorite		
+window.external.AddFavorite( url, title); 
+}	
+else if(window.opera && window.print) { 
+
+// ADD Opera Hotlist		
+return true; } 
+function br(){
+document.write("<br>");
+}
+}
+
+function farbe1(_in, _out)
+{
+  var s="";
+  var a="→         ";
+  var e="";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe2(_in, _out)
+{
+  var s="";
+  var a="~> ";
+  var e=" ッ <~";
+  var n=_in.value.concat ( );
+    _out.value=a+n+e;
+}
+function farbe3(_in, _out)
+{
+  var s="";
+  var a="░▓██ ♥► ";
+  var e=" ◄♥ ██▓░ ";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe4(_in, _out)
+{
+  var s="";
+  var a="ιllιlı. ";
+  var e=" .ιllιlı";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+
+function farbe5(_in, _out)
+{
+  var s="";
+  var a="» ";
+  var e=" 【ツ】";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe6(_in, _out)
+{
+  var s="";
+  var a="->• ";
+  var e=" •<-";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+
+function farbe7(_in, _out)
+{
+  var s="";
+  var a="·•●*´ ";
+  var e=" `*●•·˙";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe8(_in, _out)
+{
+  var s="";
+  var a="";
+  var e=" ᶫᵒᵛᵉˢᵧₒᵤ ღ";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe9(_in, _out)
+{
+  var s="";
+  var a="★•• ";
+  var e=" ••★";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe10(_in, _out)
+{
+  var s="";
+  var a="(-.(-.(-.(-";
+  var e="-).-).-).-)";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe11(_in, _out)
+{
+  var s="";
+  var a="(¯`·._.·[";
+  var e="]·._.·´¯)";
+  var n=_in.value.concat ( );
+    _out.value=a+n+e;
+}
+function farbe12(_in, _out)
+{
+  var s="";
+  var a="°o.O ";
+  var e=" O.o°";
+  var n=_in.value.concat ( );
+    _out.value=a+n+e;
+}
+function farbe13(_in, _out)
+{
+  var s="";
+  var a="`·.¸¸.·´´¯`··._.·";
+  var e="`·.¸¸.·´´¯`··._.·";
+  var n=_in.value.concat ( );
+    _out.value=a+n+e;
+}
+function farbe14(_in, _out)
+{
+  var s="";
+  var a="(¯`·._)";
+  var e="(¯`·._)";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe15(_in, _out)
+{
+  var s="";
+  var a=".·´¯`·->";
+  var e="<-·´¯`·.";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe16(_in, _out)
+{
+  var s="";
+  var a="~²º¹²~";
+  var e="~²º¹²~";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe17(_in, _out)
+{
+  var s="";
+  var a="»-(¯`v´¯)-»";
+  var e="«-(¯`v´¯)-«";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe18(_in, _out)
+{
+  var s="";
+  var a="ılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılı ";
+  var e=" ılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılı";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe19(_in, _out)
+{
+  var s="";
+  var a="...(¯`•´¯)___(¯`•´¯)__";
+  var e="__(¯`•´¯)___(¯`•´¯)...";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe20(_in, _out)
+{
+  var s="";
+  var a="((¯*.».( ¯*♥ » ";
+  var e=" « ♥* ¯).«.*¯))";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe21(_in, _out)
+{
+  var s="";
+  var a="̿̿̿̿̿ ̿̿̿̿̿̿’̿\̵͇̿̿\=((•̪●))=/̵͇̿̿/’̿̿ ̿ ̿ ̿ ";
+  var e=" ̿̿̿̿̿ ̿̿̿̿̿̿’̿\̵͇̿̿\=((•̪●))=/̵͇̿̿/’̿̿ ̿ ̿ ̿";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe22(_in, _out)
+{
+  var s="";
+  var a="°̅-̅-̅-’̿\̵͇̿̿\°»";
+  var e="«·°/̵͇̿̿/’̿-̅-̅-̅°";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe23(_in, _out)
+{
+  var s="";
+  var a="(•̪●))=/̵͇̿̿/’̿̿ ̿  ̿ ̿";
+  var e="«·°/̵͇̿̿/’̿-̅-̅-̅°";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe24(_in, _out)
+{
+  var s="";
+  var a="°̅-̅-̅-’̿\̵͇̿̿\°»`·.¸¸.·´´¯`··._.·";
+  var e="`·.¸¸.·´´¯`··._. «·°/̵͇̿̿/’̿-̅-̅-̅°";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+function farbe25(_in, _out)
+{
+  var s="";
+  var a="▂▃▄▅▆▇█▓▒░ ";
+  var e=" ░▒▓█▇▆▅▄▃▂";
+  var n=_in.value.concat ( );
+  _out.value=a+n+e;
+}
+
+function clearall(){
+
+ document.forms['nickstyler'].reset();
+
+ document.forms['nickstyler'].textin.value = '';
+
+ }
+ 
+ function xxx() {
+
+	var text = document.forms['nickstyler'].textin.value;
+	var result = new Array();
+	for (var i = 0; i <= text.length-1; i++) {
+		result[i] = '\u0305'+text[i]+'\u0305';
+	}
+	document.forms['nickstyler'].value = result.join('');
+	document.forms['nickstyler'].textin.value = result.join('');
+
+}
+ function selectText(e){var t=document,n=t.getElementById(e),r,i;if(t.body.createTextRange){r=t.body.createTextRange();r.moveToElementText(n);r.select()}else if(window.getSelection){i=window.getSelection();r=t.createRange();r.selectNodeContents(n);i.removeAllRanges();i.addRange(r)}}
+ 
